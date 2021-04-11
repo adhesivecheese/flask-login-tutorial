09aa034b96d036d2b5883e2a65744c7a3d7937d4
Cheese <adhesivecheese@gmail.com>
2021-04-10 22:29:54 -0400

    Step 5

---
+++ project/models.py
@@ -0,0 +1,7 @@
+from . import db
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+	email = db.Column(db.String(100), unique=True)
+	password = db.Column(db.String(100))
+	name = db.Column(db.String(1000))
